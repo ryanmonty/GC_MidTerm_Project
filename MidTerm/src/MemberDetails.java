bbc3e530b69743cc640545df2977b93334e0b0c1
ajabs226 <aj_abs226@yahoo.com>
2022-05-23 21:48:29 -0400

    push

--- MidTerm/src/MemberDetails.java
+++ MidTerm/src/MemberDetails.java
@@ -1,4 +1,23 @@
 
-public class MemberDetails {
+public abstract class MemberDetails {
 
+	private String name;
+	private int id;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public abstract void checkIn(Club club);
+	
+	
 }
